af9b8a99166c43b3c73be3592eea4282e234871a
jmikk <buldog.buldog26@gmail.com>
2025-04-13 13:18:48 -0500

    Update NationStatesSSE.py

--- NationStatesSSE/NationStatesSSE.py
+++ NationStatesSSE/NationStatesSSE.py
@@ -23,7 +23,7 @@ class NationStatesSSE(commands.Cog):
     @commands.admin()
     @commands.command()
     async def setchannel(self, ctx, channel: discord.TextChannel):
-        await self.config.channel.set(channel.id)
+        await self.config.set("channel", channel.id)
         await ctx.send(f"Set event output channel to {channel.mention}")
 
     @commands.admin()
@@ -195,4 +195,3 @@ class NationStatesSSE(commands.Cog):
 
         except Exception as e:
             print("[Event Handler] Error:", e)
-
