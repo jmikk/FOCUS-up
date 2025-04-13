97c79b3bc0dd98e6c5b5779036c612a8c5820f78
jmikk <buldog.buldog26@gmail.com>
2025-04-13 13:16:52 -0500

    Update NationStatesSSE.py

--- NationStatesSSE/NationStatesSSE.py
+++ NationStatesSSE/NationStatesSSE.py
@@ -82,8 +82,8 @@ class NationStatesSSE(commands.Cog):
 
     @commands.command()
     async def sstatus(self, ctx):
-        region = await self.config.region()
-        agent = await self.config.user_agent()
+        region = await self.config.region() or None
+        agent = await self.config.user_agent() or None
         channel_id = await self.config.channel() or None
         channel = self.bot.get_channel(channel_id) if channel_id else None
         running = self.sse_task and not self.sse_task.done()
