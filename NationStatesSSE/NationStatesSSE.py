3550321895de73633811bc431ab13311a82764ed
jmikk <buldog.buldog26@gmail.com>
2025-04-13 14:44:39 -0500

    Update NationStatesSSE.py

--- NationStatesSSE/NationStatesSSE.py
+++ NationStatesSSE/NationStatesSSE.py
@@ -124,7 +124,7 @@ class NationStatesSSE(commands.Cog):
             message = re.sub(r"@@(.*?)@@", lambda m: f"[{m.group(1)}](https://www.nationstates.net/nation={m.group(1).replace(' ', '_')})", message)
             message = re.sub(r"%%(.*?)%%", lambda m: f"[{m.group(1)}](https://www.nationstates.net/region={m.group(1).replace(' ', '_')})", message)
 
-            message = message.replace("&quot;",'"')
+            message = message.replace("&quot;",'"').replace("Following new legislation in","In")
             # Special handling for RMB messages
             rmb_match = re.search(r'<a href="/region=(.*?)/page=display_region_rmb\?postid=(\d+)', html)
             if rmb_match:
